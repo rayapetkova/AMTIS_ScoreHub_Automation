00de58267a2909112f389ae92e8066f351fbcaea
Raya Petkova <rayapetkova2007@abv.bg>
2026-03-09 22:23:06 +0200

    Add automation with pandas

---
+++ automation.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import sys
+import os
+
+
+
+def main():
+
+    file_paths = sys.argv[1:]
+    dataframes = []
+
+    for file in file_paths:
+        if not os.path.exists(file):
+            print(f"Файлът {file} не съществува.")
+            return
+
+        df = pd.read_csv(file)
+        df = df.set_index("ID")
+        dataframes.append(df)
+
+    categories = [
+        col for col in dataframes[0].columns
+        if col not in ["Име", "Резултат"]
+    ]
+
+    merged_df = pd.DataFrame()
+    merged_df["Име"] = dataframes[0]["Име"]
+
+    for category in categories:
+        merged_df[category] = sum(df[category] for df in dataframes) / len(dataframes)
+
+    merged_df["Резултат"] = merged_df[categories].sum(axis=1)
+    merged_df = merged_df.sort_values(by="Резултат", ascending=False)
+
+    merged_df.insert(0, "Място", range(1, len(merged_df) + 1))
+    merged = merged_df.reset_index()
+
+    merged.to_excel("final_ranking.xlsx", index=False)
+    print("Готово! Създаден е final_ranking.xlsx")
+
+
+if __name__ == "__main__":
+    main()
